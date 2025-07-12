1a726a1523d80d29194a925441b133e5e6b62a19
sid08 <sandeep2005raghu@gmail.com>
2025-07-12 19:43:40 +0530

    add dotenv

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
-import dotenv from "dotenev"
+import dotenv from "dotenv"
 
 dotenv.config();
 const app=express();
